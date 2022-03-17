05fac36ce2473de730f7460527b9a5f22ffff471
alejandroguerravillegas <coreartalex2@gmail.com>
2022-03-17 13:03:56 -0700

    Worked on isValidMove & getAvailableMoves & a little of Move methods from King

--- src/chessgame/King.java
+++ src/chessgame/King.java
@@ -1,18 +1,60 @@
 package chessgame;
 
+import java.util.LinkedList;
+import java.util.List;
+
 public class King extends Piece{
 
-    public King(boolean color) {
-        super(color);
+    // List for available moves method
+    List<String> availableMoves = new LinkedList<>();
+
+    public King(boolean color, Position position) {
+        super(color, position);
     }
 
     @Override
-    public void move() {
-        // The King is able to move one space at a time in any direction
-        //I need to know how I'm going to establish its original position
-        // And I need to get the movement from the user by using a scanner
-        // Then I add a validation of the position class to review if it's free.
+    public boolean isValidMove(Position position) {
+        int currentPositionRow = super.position.getRow();
+        int currentPositionCol = super.position.getColumn();
+        // conditional (simplified by Intellij)
+        // checking all the positions around the king.
+        return Math.abs(position.getRow() - currentPositionRow) == 1 &&
+                Math.abs(position.getColumn() - currentPositionCol) == 1 ||
+                Math.abs(position.getColumn() - currentPositionCol) == 1
+                        && position.getRow() == currentPositionRow ||
+                Math.abs(position.getRow() - currentPositionRow) == 1 &&
+                position.getColumn() == currentPositionCol;
     }
+
+    @Override
+    public void getAvailableMoves(String boardPosition) {
+        // 2d array for king's full range of movement
+        Integer [][] availableMovesKing = {{0,1,2},{0,1,2},{0,1,2}};
+        for (int i = 0; i < 2; i ++){
+            for (int j = 0; j < 2; j ++){
+                String availableMove = String.valueOf(availableMovesKing[i][j]);
+                this.availableMoves.add(availableMove);
+            }
+        }
+        System.out.print("Possible moves for " + boardPosition + ":\n" + availableMoves);
+    }
+
+    @Override
+    public void move(Position position, Position newPosition) {
+        // King movement is a 2D array where King is always in the 1,1 position
+        // and the array moves across the board basically
+        // King is allowed to move only one square at a time in any direction.
+
+        //conditional to first validate if movement is possible
+        if (isValidMove(position)){
+            // conditionals to establish the movement input from player
+            if (position.getRow() - currentPositionRow = 1){
+
+            }
+        }
+    }
+
+
 
     @Override
     public String toString() {
